8408bf2e975e7f50a4f552374d4f930be78a3ffb
YanBingxin <775127481@qq.com>
2016-07-01 09:37:36 +0800

    上传方案
    数独求解，2016-7-1 09:37:11

---
+++ 饼饼爱稳稳数独求解/饼饼爱稳稳数独求解/RelayCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace 饼饼爱稳稳数独求解
+{
+    class RelayCommand:ICommand
+    {
+        private Action m_execute;
+        private Func<bool> m_canexecute;
+        public RelayCommand(Action execute,Func<bool> canexecute)
+        {
+            m_execute = execute;
+            m_canexecute = canexecute;
+        }
+        public RelayCommand(Action execute)
+        {
+            m_execute = execute;
+        }
+        public bool CanExecute(object parameter)
+        {
+            if (m_canexecute!=null)
+            {
+                return m_canexecute();
+            }
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            m_execute();
+        }
+        public void RaiseCanExecuteChanged()
+        {
+            if (CanExecuteChanged!=null)
+            {
+                this.CanExecuteChanged(this,EventArgs.Empty);
+            }
+        }
+    }
+}
